c25847ce3d6c9ca7bb2162f3fed5931c6ea9de75
Irshath <irshath700@gmail.com>
2022-11-12 15:48:49 +0530

    create and implement blog posts list

---
+++ components/BlogPost.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+interface Props {
+	title: string;
+	excerpt: string;
+	date: string;
+	slug: string;
+}
+
+export default function BlogPost({ title, excerpt, date, slug }: Props) {
+	return (
+		<article className="w-fit p-6 transition-colors lg:m-2 lg:grid lg:grid-cols-[220px_1fr]   max-lg:hover:bg-black-200/50 dark:max-lg:hover:bg-black-700/70">
+			<div className="mb-4  border border-transparent border-l-gray-300 pl-2   dark:border-l-gray-800 lg:m-4 lg:mb-0 lg:p-2 lg:pl-4">
+				<time className="text-sm text-gray-600 dark:text-gray-400 lg:text-base">
+					{date}
+				</time>
+			</div>
+
+			<Link href={`/blog/${slug}`}>
+				<div className="rounded-xl  transition-colors duration-300 lg:p-6 lg:hover:bg-black-200/70 lg:dark:hover:bg-black-800/40">
+					<h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
+						{title}
+					</h2>
+					<p className="my-2 text-gray-600 dark:text-gray-400">{excerpt}</p>
+					<div className="mt-4 flex items-center font-medium text-teal-500 dark:text-teal-400">
+						<span>Read post</span>
+						<IoIosArrowRoundForward className="h-5 w-5" />
+					</div>
+				</div>
+			</Link>
+		</article>
+	);
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,6 @@
 import { InferGetStaticPropsType } from "next";
 import getPosts from "@/utils/getPosts";
+import BlogPost from "@/components/BlogPost";
 
 export const getStaticProps = async () => {
 	const posts = getPosts();
@@ -12,5 +13,18 @@ export const getStaticProps = async () => {
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function Blog({ posts }: Props) {
-	return <div>blog</div>;
+	return (
+		<div className="my-8">
+			<h1 className="my-8 mx-6 text-2xl font-bold text-gray-800 dark:text-gray-200 lg:mx-12">
+				All Posts
+			</h1>
+			{posts.map(({ slug, frontmatter }) => {
+				const { title, excerpt, date } = frontmatter;
+
+				return (
+					<BlogPost title={title} excerpt={excerpt} date={date} slug={slug} />
+				);
+			})}
+		</div>
+	);
 }
