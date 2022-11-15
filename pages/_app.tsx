e352e8bf046998cbfa009ceb9202e8869f6cabbb
Irshath <irshath700@gmail.com>
2022-11-15 16:15:49 +0530

    feat: use vercel's font optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
+import { Inter } from "@next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 
+const inter = Inter();
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<div className={inter.className}>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
-		</>
+		</div>
 	);
 }
