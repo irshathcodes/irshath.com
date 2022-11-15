3f7d583243bf05726468f2d273e4209af395969c
Irshath <irshath700@gmail.com>
2022-11-15 16:47:12 +0530

    fix: change heading tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,9 @@ export default function Home(props: Props) {
 			<BlogOverview posts={props.posts} />
 			<Contact />
 
-			<h6 className="my-4 text-2xl font-semibold text-gray-800 dark:text-gray-200">
+			<h1 className="my-4 text-2xl font-semibold text-gray-800 dark:text-gray-200">
 				Want to know more?{" "}
-			</h6>
+			</h1>
 			<p className="prose mb-8 text-lg text-gray-700 prose-a:font-bold prose-a:text-teal-500 dark:prose-invert dark:text-gray-300">
 				You can read my <Link href="/blog">blog</Link>, look at my{" "}
 				<Link href="/projects">projects</Link>, or learn more{" "}
